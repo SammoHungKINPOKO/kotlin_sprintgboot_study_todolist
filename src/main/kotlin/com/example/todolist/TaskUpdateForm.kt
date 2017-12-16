efb653882d689eec8395ae85330ff6d43277e3ea
SammoHungKINPOKO <dpa4o13@yahoo.com>
2017-12-17 03:14:17 +0900

    エラーメッセージの更新

--- src/main/kotlin/com/example/todolist/TaskUpdateForm.kt
+++ src/main/kotlin/com/example/todolist/TaskUpdateForm.kt
@@ -6,7 +6,7 @@ import javax.validation.constraints.Size
 class TaskUpdateForm {
 
     @NotBlank
-    @Size(max = 20)
+    @Size(max = 20, message = "20文字以内で入力しろカス")
     var content: String? = null
 
     var done: Boolean = false
